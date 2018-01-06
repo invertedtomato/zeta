9788806ade8d293062db0af853aa690c1d446354
Ben Thompson <ben@invertedtomato.com>
2018-01-06 12:35:51 +1000

    Started fleshing out client.

--- Library/WebPubSubClient.cs
+++ Library/WebPubSubClient.cs
@@ -1,16 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.WebSockets;
 using System.Text;
+using System.Threading;
 
 namespace InvertedTomato.WebPubSub {
     public class WebPubSubClient : IDisposable {
+        private readonly ClientWebSocket Socket;
+
         public Boolean IsDisposed { get; private set; }
 
-        public WebPubSubClient(String endPoint) : this(endPoint, new UInt64[] { 0 }, new Byte[] { }) { }
+        public WebPubSubClient(String endpoint) : this(endpoint, new UInt64[] { 0 }, new Byte[] { }) { }
 
-        public WebPubSubClient(String endPoint, UInt64[] groups, Byte[] authorization) {
-            throw new NotImplementedException();
+        public WebPubSubClient(String endpoint, UInt64[] channels, Byte[] authorization) {
+            if(null == endpoint) {
+                throw new ArgumentNullException(nameof(endpoint));
+            }
+            if(null == channels) {
+                throw new ArgumentNullException(nameof(channels));
+            }
+            if(null == authorization) {
+                throw new ArgumentNullException(nameof(authorization));
+            }
+
+            // Connect to server
+            var target = $"{endpoint}?channels={String.Join(",", channels)}&authorization={BitConverter.ToString(authorization)}";
+            Socket = new ClientWebSocket();
+            Socket.ConnectAsync(new Uri(target), CancellationToken.None).RunSynchronously();
+
+            StartReceive();
         }
+
+        private async void StartReceive() {
+            try {
+                while(!IsDisposed) {
+                    //await Socket.ReceiveAsync();
+
+                    // TODO
+                }
+            } catch(ObjectDisposedException) {
+            } finally {
+                Socket.Dispose();
+            }
+        }
+
         public void Subscribe<TMessage>(Action<UInt64, UInt64, TMessage> handler) {
             Subscribe(handler, UInt64.MinValue, UInt64.MaxValue);
         }
